ffa4c4cc58287bce57ccdd5aaeb63c01755a1dea
Thais2707 <thais.belmonte7@gmail.com>
2024-09-21 21:21:37 -0300

    teste

--- Modulo2/Aula1.js
+++ Modulo2/Aula1.js
@@ -8,3 +8,7 @@ console.log ("Hello Word")
 
 // git cat-file -p - da as caracteristicas do commit
 
+// touch "nome do arquivo" - comando para criar arquivos direto do terminal ainda não esta sendo verificado pelo git 
+// git add "nome do arquivo" - coloca o arquivo na area de reconhecimento do git
+
+// echo "conteudo">>"nome do arquivo" - preenche o arquivo direto do terminal

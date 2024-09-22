7064ac69e08a7986a720e00ee1a6caf227f47ff6
Thais2707 <thais.belmonte7@gmail.com>
2024-09-21 21:11:30 -0300

    Aula 19

--- Modulo2/Aula1.js
+++ Modulo2/Aula1.js
@@ -1,1 +1,10 @@
-teste
+console.log ("Hello Word")
+
+// git hist - verifica o historico de comits
+
+// git log - mostra os logs do git
+
+// git cat-file -t - da o tipo de commit
+
+// git cat-file -p - da as caracteristicas do commit
+

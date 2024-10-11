8f1d2da2a6266a6e24f7daa62a596cadc833f472
Thais2707 <thais.belmonte7@gmail.com>
2024-10-10 21:49:16 -0300

    Aula 27

---
+++ HTML-CSS-JS/4-js-basic/DOM/Exer_Adicioner_e_remover itens/script.js
@@ -0,0 +1,33 @@
+let lista = document.getElementById("listaItens");
+let mensagem = document.getElementById("mensagem");
+let contador = 1;
+
+function exibirMensagem(texto, cor) {
+    mensagem.innerHTML = texto;
+    mensagem.style.display = "block";
+    mensagem.style.backgroundColor = cor;
+}
+
+function adicionarItem() {
+    let novoItem = document.createElement("li");
+    novoItem.innerHTML = `Item ${contador}`;
+    lista.appendChild(novoItem);
+    contador++;
+    exibirMensagem("Item adicionado", "lightblue");
+}
+
+function removerItem() {
+    if (lista.children.length > 0) {
+        let indice = prompt("Digite o número do item que deseja remover:");
+        let itemRemover = document.querySelector(`li:nth-child(${indice})`); // Corrigido para usar template string
+
+        if (itemRemover) {
+            lista.removeChild(itemRemover);
+            exibirMensagem("Item removido", "lightcoral");
+        } else {
+            alert("Item não encontrado!");
+        }
+    } else {
+        exibirMensagem("Não há mais itens para remover", "grey");
+    }
+}
